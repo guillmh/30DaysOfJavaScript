00650ea85cef558005edbfc9bc98212839f608b6
«Guillber» <guillbermndz@gmail.com>
2024-10-04 22:02:38 -0600

    day 08 update

--- 30-Days-Of-JavaScript/Day_8/day08.js
+++ 30-Days-Of-JavaScript/Day_8/day08.js
@@ -20,3 +20,68 @@ dog.breed = 'Pitbull';
 dog.getDogInfo = function(){
     return `My dog is: ${this.name} the color is: ${this.color}`};
 console.log(dog);
+//Ejercicios: Nivel 2
+const users = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+  }
+//1.-Encuentra a la persona que tiene muchas habilidades en el objeto de los usuarios.
+let maxSkills = 0;
+let nameUserSkills = '';
+for(let user in users){
+  let userSkills = users[user].skills;
+  // Contar cuántas habilidades tiene el usuario
+  let numberOfSkills = userSkills.length;
+  if(numberOfSkills > maxSkills){
+    maxSkills = numberOfSkills
+    nameUserSkills = user;
+  }
+}
+console.log(`${nameUserSkills} tiene mayores habilidades con un total${maxSkills}`)
