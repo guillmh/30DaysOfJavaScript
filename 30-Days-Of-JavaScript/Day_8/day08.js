376c75880af45cb8a708e624e07b9d5f6673d9aa
«Guillber» <guillbermndz@gmail.com>
2024-10-09 20:52:02 -0600

    Day 08 update

--- 30-Days-Of-JavaScript/Day_8/day08.js
+++ 30-Days-Of-JavaScript/Day_8/day08.js
@@ -116,4 +116,174 @@ for(stack in users){
    console.log(`${stack} cumple con el stack MERN:${mernStack}`)
   }
 }
-//explica el codigo
+//4.-Establezca su nombre en el objeto usuarios sin modificar el objeto usuarios original
+const copyUsers = Object.assign({}, users);
+copyUsers.Guillber = {
+  email: 'guill@guill.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 24,
+      isLoggedIn: false,
+      points: 30
+};
+console.log(copyUsers);
+//5.-Obtener todas las claves o propiedades del objeto usuarios
+console.log(copyUsers);
+//6.-Obtener todos los valores del objeto usuarios
+const valueUsers = Object.values(copyUsers);
+console.log(valueUsers);
+//7.-Utilice el objeto países para imprimir el nombre del país, la capital, la población y los idiomas.
+const paises = {
+  Mexico: {
+    nombre: "México",
+    capital: "Ciudad de México",
+    poblacion: 128932753,
+    idiomas: ["Español"]
+  },
+  Canada: {
+    nombre: "Canadá",
+    capital: "Ottawa",
+    poblacion: 37742154,
+    idiomas: ["Inglés", "Francés"]
+  },
+  Japon: {
+    nombre: "Japón",
+    capital: "Tokio",
+    poblacion: 126476461,
+    idiomas: ["Japonés"]
+  },
+  Alemania: {
+    nombre: "Alemania",
+    capital: "Berlín",
+    poblacion: 83783942,
+    idiomas: ["Alemán"]
+  },
+  Brasil: {
+    nombre: "Brasil",
+    capital: "Brasilia",
+    poblacion: 212559417,
+    idiomas: ["Portugués"]
+  }
+};
+for(country in paises){
+let countryNumber = paises[country];
+console.log(`${countryNumber.nombre} - ${countryNumber.capital} - ${countryNumber.poblacion} - ${countryNumber.idiomas}`)
+}
+//Ejercicios: Nivel 3
+//1.-Crea un objeto literal llamado personAccount. Tiene las propiedades firstName, lastName, incomes, expenses y tiene los metodos totalIncome, totalExpense, accountInfo,addIncome, addExpense y accountBalance. Incomes es un conjunto de ingresos y su descripción y expenses es un conjunto de ingresos y su descripción
+const personAccount = {
+  FisrtName: "Alexa",
+  LastName: "Mendoza",
+  Incomes: {
+    Spotify: 120,
+    desc1: 'Servicio de musica',
+    Food: 500,
+    desc2: 'Comida',
+    Amazon: 800,
+    desc3: 'Necesidades basicas',
+  },
+  Expense: {
+    Youtube: 1000,
+    info1: 'Ingreosos publicitarios de youtube',
+    Salary:  2000,
+    info2: 'Salario semanal',
+  },
+  totalIncome: function () {
+
+  },
+  totalExpense: function () {
+
+  },
+  accountInfo: function () {
+
+  },
+  addIncome: function () {
+
+  },
+  addExpense: function (){
+
+  },
+  accountBalance: function (){
+
+  }
+}
+//2.-**** Preguntas:2, 3 y 4 se basan en los siguientes dos arrays: users y products ()
+const usersTag = [
+  {
+    _id: "ab12ex",
+    username: "Alex",
+    email: "alex@alex.com",
+    password: "123123",
+    createdAt: "08/01/2020 9:00 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "fg12cy",
+    username: "Asab",
+    email: "asab@asab.com",
+    password: "123456",
+    createdAt: "08/01/2020 9:30 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "zwf8md",
+    username: "Brook",
+    email: "brook@brook.com",
+    password: "123111",
+    createdAt: "08/01/2020 9:45 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "eefamr",
+    username: "Martha",
+    email: "martha@martha.com",
+    password: "123222",
+    createdAt: "08/01/2020 9:50 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "ghderc",
+    username: "Thomas",
+    email: "thomas@thomas.com",
+    password: "123333",
+    createdAt: "08/01/2020 10:00 AM",
+    isLoggedIn: false,
+  },
+];
+
+const products = [
+  {
+    _id: "eedfcf",
+    name: "mobile phone",
+    description: "Huawei Honor",
+    price: 200,
+    ratings: [
+      { userId: "fg12cy", rate: 5 },
+      { userId: "zwf8md", rate: 4.5 },
+    ],
+    likes: [],
+  },
+  {
+    _id: "aegfal",
+    name: "Laptop",
+    description: "MacPro: System Darwin",
+    price: 2500,
+    ratings: [],
+    likes: ["fg12cy"],
+  },
+  {
+    _id: "hedfcg",
+    name: "TV",
+    description: "Smart TV:Procaster",
+    price: 400,
+    ratings: [{ userId: "fg12cy", rate: 5 }],
+    likes: ["fg12cy"],
+  },
+];
+
+const signUp = (newUser) =>{
+const copyList = Object.assign({}, usersTag);
+copyList.newUser = {
+  //vacio
+}
+}
+console.log(usersTag)
