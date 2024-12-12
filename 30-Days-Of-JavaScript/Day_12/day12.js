4ed8fedc8a853ef7088952b16614b95eac984df7
guillbermendez <guillbermndz@gmail.com>
2024-12-11 22:11:34 -0600

    Day 12 update

--- 30-Days-Of-JavaScript/Day_12/day12.js
+++ 30-Days-Of-JavaScript/Day_12/day12.js
@@ -3,7 +3,7 @@
 let incomeCash = 'Gana 4000 euros de sueldo al mes, 10000 euros de bonificación anual, 5500 euros de cursos online al mes';
 const yearCash = (text) => {
     let searchText = /\d+/g;
-    let diner = text.match(searchText)
+    let diner = text.match(searchText);
     let salary = diner[0] * 12;
     let bonuses = diner[1];
     let courses = diner[2] * 12;
@@ -11,3 +11,12 @@ const yearCash = (text) => {
 
 }
 console.log(yearCash(incomeCash));
+//2.-La posición de algunas partículas en el eje horizontal x -12, -4, -3 y -1 en la dirección negativa, 0 en el origen, 4 y 8 en la dirección positiva. Extrae estos números y encuentra la distancia entre las dos partes más lejanas.
+let textNum = 'La posición de algunas partículas en el eje horizontal x -12, -4, -3 y -1 en la dirección negativa, 0 en el origen, 4 y 8 en la dirección positiva';
+const searchNumbers = (text) => {
+let searchNum = /-?\d+/g;
+let nums = text.match(searchNum);
+const sortedPoints = nums.map(Number).sort((a, b) => a - b);
+return sortedPoints;
+}
+console.log(searchNumbers(textNum));
