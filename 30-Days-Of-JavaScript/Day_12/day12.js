4c8c2e13cb37183af90a4442698741433e3cc041
guillbermendez <guillbermndz@gmail.com>
2024-12-12 22:03:17 -0600

    day 12 update

--- 30-Days-Of-JavaScript/Day_12/day12.js
+++ 30-Days-Of-JavaScript/Day_12/day12.js
@@ -14,9 +14,46 @@ console.log(yearCash(incomeCash));
 //2.-La posición de algunas partículas en el eje horizontal x -12, -4, -3 y -1 en la dirección negativa, 0 en el origen, 4 y 8 en la dirección positiva. Extrae estos números y encuentra la distancia entre las dos partes más lejanas.
 let textNum = 'La posición de algunas partículas en el eje horizontal x -12, -4, -3 y -1 en la dirección negativa, 0 en el origen, 4 y 8 en la dirección positiva';
 const searchNumbers = (text) => {
-let searchNum = /-?\d+/g;
-let nums = text.match(searchNum);
-const sortedPoints = nums.map(Number).sort((a, b) => a - b);
-return sortedPoints;
+let nums = text.match(/-?\d+/g).map(Number);
+// 2. Encontrar el valor mínimo y máximo
+const min = Math.min(...nums);
+const max = Math.max(...nums);
+// 3. Calcular la distancia
+const distance = Math.abs(max - min);
+return `${nums} ${distance}`;
 }
 console.log(searchNumbers(textNum));
+//3.-Escribir un patrón que identifique si una cadena es una variable JavaScript válida
+const validatedString = (text) => {
+    let pattern = /^[a-zA-Z_][a-zA-Z_]*$/;
+    let validated = pattern.test(text)
+    return validated;
+}
+console.log(validatedString('2first-name'));
+//Ejercicios: Nivel 2
+//1.-Escriba una función llamada tenMostFrequentWords que obtenga las diez palabras más frecuentes de una cadena?
+let paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`;
+
+const tenMostFrequentWords = (text, num) => {
+const search = /[a-zA-Z]+/g;
+const searchWords = text.match(search).map((word) => word.toLowerCase());
+// Crear un Map para almacenar las frecuencias de las palabras
+const wordMap = new Map();
+// Contar las frecuencias
+ searchWords.forEach(word => {
+    if (wordMap.has(word)) {
+      wordMap.set(word, wordMap.get(word) + 1);  // Incrementar la frecuencia
+    } else {
+      wordMap.set(word, 1);  // Inicializar la frecuencia
+    }
+  });
+// Convertir el Map a un array y ordenar por frecuencia
+ const sortedWords = Array.from(wordMap.entries()).sort((a, b) => b[1] - a[1]);  // Ordenar por la frecuencia (valor) de mayor a menor
+ // Tomar las 10 palabras más frecuentes
+ const top10Words = sortedWords.slice(0, num).map(entry => {
+    return { word: entry[0], frequency: entry[1] };  // Crear un objeto con la palabra y su frecuencia
+  });
+
+  return top10Words;
+}
+console.log(tenMostFrequentWords(paragraph, 10));
