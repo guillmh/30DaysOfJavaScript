59dd86047fbeb63f65daa4627176b865f3b5e95b
guillbermendez <guillbermndz@gmail.com>
2025-01-17 22:43:59 -0600

    day 23 update

--- 30-Days-Of-JavaScript/Day_22/ejercicio2/main.js
+++ 30-Days-Of-JavaScript/Day_22/ejercicio2/main.js
@@ -1,1 +1,12 @@
+const countriesAPI = "https://restcountries.com/v2/all";
 
+const getApi = async () => {
+    try{
+    const response = await fetch(countriesAPI);
+    const dataApi =  await response.json();
+    console.log(dataApi);
+    }catch(error){
+        console.log(error)
+    }
+};
+getApi()
