5d0b064b3fc32005855ee500c6f5106db1731021
guillbermendez <guillbermndz@gmail.com>
2025-01-23 22:03:00 -0600

    day 22 finished

--- 30-Days-Of-JavaScript/Day_22/ejercicio2/main.js
+++ 30-Days-Of-JavaScript/Day_22/ejercicio2/main.js
@@ -5,15 +5,32 @@ const getApi = async () => {
     const response = await fetch(countriesAPI);
     const dataApi =  await response.json();
     dataApi.map((countrie) => {
+        //Obtiene el div que contendra os paises
         let boxCountries = document.getElementById('boxCountries');
+        let getCountrie = document.getElementById('numberCountries');
+        //Crea un div donde iran los nombres de los paises
         const countrieData = document.createElement('div');
+        //Agrega los div a la caja de paises
         boxCountries.appendChild(countrieData);
+        //Agrega texto el cual es el nombre los paises 
         countrieData.textContent = countrie.name;
+        //Obtiene el numero de paises
+        getCountrie.textContent = dataApi.length;
+        //Estilos para el div de cada pais
+        countrieData.style.display = 'flex';
+        countrieData.style.width = '135px';
+        countrieData.style.height ='135px'
+        countrieData.style.justifyContent = 'center';
+        countrieData.style.alignItems = 'center';
+        countrieData.style.textAlign = 'center';
+        countrieData.style.border = '1px solid #f1f1ef';
+        countrieData.style.borderRadius = '5px';
+        countrieData.style.margin = '5px';
     });
     }catch(error){
         console.log(error)
     }
 };
 getApi()
-//Explica el codigo y porsteiormente agrega estilo en CSS
+
 
